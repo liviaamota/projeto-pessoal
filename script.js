e6aaba1ba9af4a2bb31adf0a5ce0a2952cd759c5
liviaamota <livia.mota11@aluno.ce.gov.br>
2024-11-14 08:48:23 -0300

    up

---
+++ script.js
@@ -0,0 +1,25 @@
+let option = ''
+ // o projeto deve contar sobre posiçoes,regras e quantidade de jogadores.
+const handball = {}
+ do {
+    option = prompt(`
+    1 - handebool
+    2 -  sair 
+
+    `)
+    
+    switch (option){
+        case '1': 
+        handball.positions = alert('Existem cinco posições no handebool,quais delas são central,ponta,pivÔ,armação e goleiro')
+        handball.rules = alert('Tempo de posse: não há limite de tempo, mas ataques passivos são penalizados. Substituições: as substituições de jogadores são ilimitadas .')
+        handball.players = alert ('A quantidade de jogadores são sete,contando com o goleiro.')
+             
+           
+            break
+        case'2': 
+        alert('Saindo, obrigado pela atenção ...')
+    
+    }
+} while (option !== '2')
+
+    
